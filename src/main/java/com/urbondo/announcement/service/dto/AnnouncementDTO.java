bc1efaf4f9683e9e1d2eff6387be22fcd474251b
Mena Bebawy <eng.mena.lorans@gmail.com>
2023-05-24 13:47:43 +0200

    add entities and their dtos

---
+++ src/main/java/com/urbondo/announcement/service/dto/AnnouncementDTO.java
@@ -0,0 +1,6 @@
+package com.urbondo.announcement.service.dto;
+
+public record AnnouncementDTO(String id,
+                              String title,
+                              String body,
+                              String categoryId) {}
---
+++ src/main/java/com/urbondo/announcement/service/dto/CategoryDTO.java
@@ -0,0 +1,3 @@
+package com.urbondo.announcement.service.dto;
+
+public record CategoryDTO(String id, String title) {}
