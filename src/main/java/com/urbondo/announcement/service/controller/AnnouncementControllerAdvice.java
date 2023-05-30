06b10d9f08e417d1152a8c5193d9d751ad6b738d
Mena Bebawy <eng.mena.lorans@gmail.com>
2023-05-30 18:24:52 +0200

    get defaultMessage from MethodArgumentNotValidException

--- src/main/java/com/urbondo/announcement/service/controller/AnnouncementControllerAdvice.java
+++ src/main/java/com/urbondo/announcement/service/controller/AnnouncementControllerAdvice.java
@@ -1,18 +1,39 @@
 package com.urbondo.announcement.service.controller;
 
 import com.urbondo.announcement.service.exception.CategoryAlreadyExistException;
+import com.urbondo.announcement.service.exception.CategoryNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class AnnouncementControllerAdvice {
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(CategoryNotFoundException.class)
+    String handleUserNotFoundException(CategoryNotFoundException exception) {
+        return createResponseBody(exception.getMessage());
+    }
+
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler({CategoryAlreadyExistException.class})
     String handleBadRequestException(Exception exception) {
         return createResponseBody(exception.getMessage());
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    String handleValidationErrors(MethodArgumentNotValidException exception) {
+        String defaultMessage = "";
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            defaultMessage = fieldError.getDefaultMessage();
+        }
+        return createResponseBody(defaultMessage);
     }
 
     private String createResponseBody(String value) {
